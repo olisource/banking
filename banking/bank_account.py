32340a594950234d55745426c9532641c1ebb0e8
olisource <arivolit@gmail.com>
2017-11-17 02:21:26 -0800

    Initial checkin

---
+++ banking/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount(object):
+    def __init__(self, balance = 3000):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance =- amount
+        else:
+            return "Invalid transaction"
+
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self, minimum = 1000):
+        self.minimum = minimum
---
+++ test_bank_account.py
@@ -0,0 +1,24 @@
+import unittest
+
+from bank_account import BankAccount, MinimumBalanceAccount
+
+class AccountBalanceTestCase(unittest.TestCase)
+    def setUp(self):
+        self.account1 = BankAccount()
+
+    def test_balance(self):
+        self.assertEqual(self.account1.balance, 3000, msg="Invalid account balance")
+
+    def test_deposit(self):
+        self.account1.deposit(4000)
+        self.assertEqual(self.account1.balance, 7000, msg="Inaccurate deposit method")
+
+    def test_withdraw(self):
+        self.account1.withdraw(500)
+        self.assertEqual(self.account1.balance, 2500, msg="Inaccurate withdraw method")
+
+    def test_invalid_transaction(self):
+        self.assertEqual(self.account1.withdraw(6000), "Invalid transaction", msg="Invalid transaction")
+
+    def test_sub_class(self):
+        self.assertTrue(issubclass(MinimumBalanceAccount, BankAccount), msg="Not a subclass of BalanceAccount")
