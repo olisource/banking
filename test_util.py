d0cffc2302ac82620f3a81cd872eeee005858fa3
olisource <arivolit@gmail.com>
2017-11-17 07:33:34 -0800

    Addtion calculator

---
+++ test_util.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+import xmlrunner
+import unittest
+
+from util import Calculator
+
+class CalculatorTestCase(unittest.TestCase)
+    def setUp(self):
+        self.calculator1 = Calculator()
+
+    def tearDown(self):
+        pass
+
+    def test_addition_success(self):
+        self.assertEqual(self.calculator1.addition(1, 3), 4)
---
+++ util/calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        return a / b
+
+    def simple_interest(self, principal, numOfYears, rate):
+        return (principal * numOfYears * rate) / 100
